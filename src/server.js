5888b6e260f0496df98770c722ca50df33cfe4b8
01jeong <{ID}+{username}@users.noreply.github.com>
2025-01-13 16:12:43 +0900

    compress requests and rate limits endpoints

--- src/server.js
+++ src/server.js
@@ -2,10 +2,22 @@ import express from 'express';
 import mainRoutes from './main.routes'
 import userRoutes from './user.routes';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import compression from 'compression';
 
 const app = express();
 const port = 3000;
 
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  // standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  // legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(compression())
+app.use(limiter)
 app.use(express.json())
 app.use(helmet())
 

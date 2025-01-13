0485e9690115adab22b02a665b4eb7e83ec1c5d6
01jeong <{ID}+{username}@users.noreply.github.com>
2025-01-13 16:16:16 +0900

    limiter changed to 10 from 100

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = 3000;
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   // standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   // legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Redis, Memcached, etc. See below.
